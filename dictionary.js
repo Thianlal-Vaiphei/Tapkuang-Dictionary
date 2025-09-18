38084907d766faf5797e1e3b4c4f015fa65dce42
Thianlal-Vaiphei <thianlal2012vaiphei@gmail.com>
2025-09-19 00:32:25 +0530

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,17 @@
+// dictionary-data.js
+// Keep arrays of strings "Left - Right" ; left is the search/sort key
+
+window.initialV2E = [
+  "Aw, ngawng-aw - Put around neck, necktie, muffler",
+  "Awle - Crocodile",
+  "Awp, awpkhum - incubate, incumbent/lie upon",
+  "Awt, Awchat - Envy, jealous, grudge",
+  "Awt, lampi awt - Give chance to other, give way"
+];
+
+window.initialE2V = [
+  "Apple - Sial",
+  "Book - Thu",
+  "Cat - Kizu",
+  "Dog - Uithu"
+];
