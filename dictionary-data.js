088b12bca308ade177596f173a3f9c49b719c563
Thianlal-Vaiphei <thianlal2012vaiphei@gmail.com>
2025-09-20 22:45:00 +0530

    Create dictionary-data.js

---
+++ dictionary-data.js
@@ -0,0 +1,25 @@
+window.initialV2E = [
+  "sial - apple",
+  "thu - book",
+  "kizu - cat",
+  "uithu - dog",
+  "ngui - fish",
+  "bawm - tree",
+  "tuipi - water",
+  "lam - road",
+  "thumal - pen",
+  "pathian - god"
+];
+
+window.initialE2V = [
+  "Apple - sial",
+  "Book - thu",
+  "Cat - kizu",
+  "Dog - uithu",
+  "Fish - ngui",
+  "Tree - bawm",
+  "Water - tuipi",
+  "Road - lam",
+  "Pen - thumal",
+  "God - pathian"
+];
